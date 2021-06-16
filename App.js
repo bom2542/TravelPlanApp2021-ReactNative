de60e350562c97c4a8563ed0c1dc559010e34a7c
Pharadorn Boonruam <61776315+bom2542@users.noreply.github.com>
2021-06-16 13:43:02 +0700

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Pharadorn Boonruam</Text>
+      <Text>Pharadorn Boonruam555555</Text>
       <StatusBar style="auto" />
     </View>
   );
